e541363174e60953d48e933cc0f2bc7193770f69
Graham Hill <gthill97@gmail.com>
2021-05-12 18:57:28 -0600

    First cut at hierarchical clustering

--- langmap.py
+++ langmap.py
@@ -9,7 +9,12 @@ import geopandas as gpd
 world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
 
 
-def plot(points):
+def plot(points, labels=None):
     fig, ax = plt.subplots(figsize=(16, 10))
     world.plot(ax=ax, color='w', edgecolor='k', linewidth=0.5)
-    points.plot(ax=ax, marker='o', color='b', markersize=5)
+    if labels is None:
+        points.plot(ax=ax, marker='o', color=color, markersize=25)
+    else:
+        labelled_points = points.copy()
+        labelled_points['label'] = labels
+        labelled_points.plot(ax=ax, marker='o', column='label', markersize=25, cmap='Set1')
